76fb25a75425948901dec43821cfb26faa703be0
CoolOppo <CoolOppo@users.noreply.github.com>
2021-12-14 14:01:36 -0500

    Hole punching works

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,17 @@
 use std::fs;
 use std::str::FromStr;
 use std::{
-    net::{Ipv4Addr, SocketAddr},
+    net::SocketAddr,
     path::{Path, PathBuf},
     time::Duration,
 };
 
 use bytes::Bytes;
 use color_eyre::eyre::Result;
+use local_ip_address::local_ip;
+
 use qp2p::{Config, Endpoint, IncomingConnections};
 use structopt::StructOpt;
-
 use tokio::spawn;
 
 #[derive(Debug, StructOpt)]
@@ -23,12 +24,8 @@ struct Opt {
     #[structopt(short, long, name = "FILE", parse(from_os_str))]
     send: Option<PathBuf>,
 
-    /// Network port
-    #[structopt(short, long)]
-    port: u16,
-
-    /// Network port
-    #[structopt(short = "o", long, parse(try_from_str = parse_socket_addr))]
+    /// Peer's network address (ip:port)
+    #[structopt(short = "o", long="ip", parse(try_from_str = parse_socket_addr))]
     peer_address: Option<SocketAddr>,
 }
 
@@ -38,51 +35,55 @@ fn parse_socket_addr(src: &str) -> Result<SocketAddr, std::net::AddrParseError> 
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // tracing_subscriber::fmt()
+    //     .with_max_level(Level::TRACE)
+    //     .init();
     color_eyre::install()?;
     let opt = Opt::from_args();
 
     if let Some(file) = opt.send {
+        // sending
         if Path::exists(&file) {
-            let tx = spawn(send(opt.port, opt.peer_address.unwrap(), fs::read(file)?));
+            let tx = spawn(send(opt.peer_address.unwrap(), fs::read(file)?));
             tx.await??;
         } else {
             panic!("File does not exist.")
         }
     } else {
-        let rx = spawn(receive(opt.port));
+        // receiving
+        let rx = spawn(receive());
         rx.await??;
     }
 
     Ok(())
 }
 
-async fn send(port: u16, peer_address: SocketAddr, file: Vec<u8>) -> Result<()> {
-    let (node, _incoming_connections) = make_node(port).await?;
+async fn send(peer_addr: SocketAddr, file: Vec<u8>) -> Result<()> {
+    let (node, _incoming_connections) = make_node().await?;
 
     let msg = Bytes::from(file);
     // println!("Sending to {:?} --> {:?}\n", peer_address, msg);
-    println!("Sending to {:?}", peer_address);
-    let (connection, _connection_incoming) = node.connect_to(&peer_address).await?;
+    println!("Sending to {:?}", peer_addr);
+    let (connection, _connection_incoming) = node.connect_to(&peer_addr).await?;
     connection.send(msg).await?;
     connection.close();
 
     Ok(())
 }
 
-async fn receive(port: u16) -> Result<()> {
+async fn receive() -> Result<()> {
     println!("Waiting for connection...");
 
-    let (_node, mut incoming_connections) = make_node(port).await?;
+    let (_node, mut incoming_connections) = make_node().await?;
 
     while let Some((connection, mut incoming_messages)) = incoming_connections.next().await {
-        let src = connection.remote_address();
-        println!("Connection made to {:?}", src);
+        println!("Connection made to {:?}", connection.remote_address());
 
         // loop over incoming messages
         while let Some(bytes) = incoming_messages.next().await? {
             // println!("Message received from {:?} --> {:?}", src, bytes);
             println!("File received; writing to output.bin.");
-            fs::write("output.bin", bytes);
+            fs::write("output.bin", bytes)?;
             println!("Done!");
         }
     }
@@ -90,12 +91,27 @@ async fn receive(port: u16) -> Result<()> {
     Ok(())
 }
 
-async fn make_node(port: u16) -> Result<(Endpoint, IncomingConnections)> {
+async fn make_node() -> Result<(Endpoint, IncomingConnections)> {
+    let internal_ip = local_ip().expect("Couldn't get internal IP.");
+    let port = port_scanner::request_open_port().expect("Unable to find an available port.");
+    let external_ip = public_ip::addr().await;
+
+    let local_addr = SocketAddr::new(internal_ip, port);
+
+    let printed_addr = {
+        if let Some(external_ip) = external_ip {
+            SocketAddr::new(external_ip, port)
+        } else {
+            local_addr
+        }
+    };
+    println!("Endpoint created at {:?}", printed_addr);
+
     let (node, incoming_connections, _) = Endpoint::new(
-        SocketAddr::from((Ipv4Addr::new(0, 0, 0, 0), port)),
+        local_addr,
         &[],
         Config {
-            // forward_port: true, // TODO: change to true when testing NAT hole punching
+            forward_port: true,
             idle_timeout: Some(Duration::from_secs(120)),
             ..Default::default()
         },
