210bfd855541fcdaefb047e7a120b887c1b6987a
CoolOppo <CoolOppo@users.noreply.github.com>
2021-12-14 12:06:26 -0500

    Works like a boge 2.0

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![allow(unused)]
 
 use std::fs;
+use std::str::FromStr;
 use std::{
     net::{Ipv4Addr, SocketAddr},
     path::{Path, PathBuf},
@@ -20,7 +21,6 @@ use tokio::{
     time::sleep,
 };
 
-
 #[derive(Debug, StructOpt)]
 #[structopt(about = "Send or receive files. Receiving is default unless the send flag is used.")]
 struct Opt {
@@ -33,8 +33,12 @@ struct Opt {
     port: u16,
 
     /// Network port
-    #[structopt(short = "o", long)]
-    peer_port: Option<u16>,
+    #[structopt(short = "o", long, parse(try_from_str = parse_socket_addr))]
+    peer_address: Option<SocketAddr>,
+}
+
+fn parse_socket_addr(src: &str) -> Result<SocketAddr, std::net::AddrParseError> {
+    SocketAddr::from_str(src)
 }
 
 #[tokio::main]
@@ -44,7 +48,7 @@ async fn main() -> Result<()> {
 
     if let Some(file) = opt.send {
         if Path::exists(&file) {
-            let tx = spawn(send(opt.port, opt.peer_port.unwrap(), fs::read(file)?));
+            let tx = spawn(send(opt.port, opt.peer_address.unwrap(), fs::read(file)?));
             tx.await??;
         } else {
             panic!("File does not exist.")
@@ -57,14 +61,13 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn send(port: u16, peer_port: u16, file: Vec<u8>) -> Result<()> {
+async fn send(port: u16, peer_address: SocketAddr, file: Vec<u8>) -> Result<()> {
     let (node, mut incoming_connections) = make_node(port).await?;
 
-    let peer: SocketAddr = SocketAddr::from((Ipv4Addr::LOCALHOST, peer_port));
     let msg = Bytes::from(file);
-    // println!("Sending to {:?} --> {:?}\n", peer, msg);
-    println!("Sending to {:?}", peer);
-    let (connection, _connection_incoming) = node.connect_to(&peer).await?;
+    // println!("Sending to {:?} --> {:?}\n", peer_address, msg);
+    println!("Sending to {:?}", peer_address);
+    let (connection, _connection_incoming) = node.connect_to(&peer_address).await?;
     connection.send(msg).await?;
     connection.close();
 
@@ -97,7 +100,7 @@ async fn make_node(port: u16) -> Result<(Endpoint, IncomingConnections)> {
         SocketAddr::from((Ipv4Addr::new(0, 0, 0, 0), port)),
         &[],
         Config {
-            forward_port: false, // TODO: change to true when testing NAT hole punching
+            // forward_port: true, // TODO: change to true when testing NAT hole punching
             idle_timeout: Some(Duration::from_secs(120)),
             ..Default::default()
         },
