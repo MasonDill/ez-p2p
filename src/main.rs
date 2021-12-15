ba17c3f950cf3c0206c7b629833cbeee1f47fafb
CoolOppo <CoolOppo@users.noreply.github.com>
2021-12-14 20:15:17 -0500

    Works on only TCP now

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,34 @@
 #![warn(clippy::all)]
 // #![allow(unused)]
 
-use std::fs;
-use std::str::FromStr;
+mod igd;
+
 use std::{
     net::SocketAddr,
     path::{Path, PathBuf},
+    str::FromStr,
     time::Duration,
 };
 
-use bytes::Bytes;
 use color_eyre::eyre::Result;
 use local_ip_address::local_ip;
 
-use qp2p::{Config, Endpoint, IncomingConnections};
+use crate::igd::{forward_port, IgdError};
 use structopt::StructOpt;
-use tokio::spawn;
+use tokio::{
+    fs,
+    fs::File,
+    io::{copy, AsyncWriteExt, BufReader, BufWriter},
+    net::{TcpListener, TcpStream},
+    spawn,
+    time::timeout,
+};
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "Send or receive files. Receiving is default unless the send flag is used.")]
 struct Opt {
-    /// Specifies which file to send. (Default is receiving if this flag is unused.)
+    /// Specifies which file to send. (Default is receiving if this flag is
+    /// unused.)
     #[structopt(short, long, name = "FILE", parse(from_os_str))]
     send: Option<PathBuf>,
 
@@ -38,16 +46,16 @@ async fn main() -> Result<()> {
     // tracing_subscriber::fmt()
     //     .with_max_level(Level::TRACE)
     //     .init();
-    color_eyre::install()?;
+    // color_eyre::install()?;
     let opt = Opt::from_args();
 
     if let Some(file) = opt.send {
         // sending
-        if Path::exists(&file) {
-            let tx = spawn(send(opt.peer_address.unwrap(), fs::read(file)?));
+        if Path::exists(&file) && fs::metadata(&file).await?.is_file() {
+            let tx = spawn(send(opt.peer_address.unwrap(), File::open(file).await?));
             tx.await??;
         } else {
-            panic!("File does not exist.")
+            panic!("File does not exist.");
         }
     } else {
         // receiving
@@ -58,40 +66,48 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn send(peer_addr: SocketAddr, file: Vec<u8>) -> Result<()> {
-    let (node, _incoming_connections) = make_node().await?;
-
-    let msg = Bytes::from(file);
-    // println!("Sending to {:?} --> {:?}\n", peer_address, msg);
+async fn send(peer_addr: SocketAddr, file: File) -> Result<()> {
     println!("Sending to {:?}", peer_addr);
-    let (connection, _connection_incoming) = node.connect_to(&peer_addr).await?;
-    connection.send(msg).await?;
-    connection.close();
+    let mut stream = TcpStream::connect(peer_addr).await?;
+    copy(&mut BufReader::new(file), &mut stream).await?;
+    stream.shutdown().await?;
+    println!("Done!");
 
     Ok(())
 }
 
 async fn receive() -> Result<()> {
-    println!("Waiting for connection...");
+    let internal_ip = local_ip().expect("Couldn't get internal IP.");
+    let port = port_scanner::request_open_port().expect("Unable to find an available port.");
+    let external_ip = public_ip::addr().await;
 
-    let (_node, mut incoming_connections) = make_node().await?;
+    let local_addr = SocketAddr::new(internal_ip, port);
 
-    while let Some((connection, mut incoming_messages)) = incoming_connections.next().await {
-        println!("Connection made to {:?}", connection.remote_address());
-
-        // loop over incoming messages
-        while let Some(bytes) = incoming_messages.next().await? {
-            // println!("Message received from {:?} --> {:?}", src, bytes);
-            println!("File received; writing to output.bin.");
-            fs::write("output.bin", bytes)?;
-            println!("Done!");
+    let printed_addr = {
+        if let Some(external_ip) = external_ip {
+            SocketAddr::new(external_ip, port)
+        } else {
+            local_addr
         }
-    }
+    };
 
+    timeout(
+        Duration::from_secs(30),
+        forward_port(port, local_addr, Duration::from_secs(60 * 60)),
+    )
+    .await
+    .map_err(|_| IgdError::TimedOut)??;
+
+    println!("Endpoint created at {:?}", printed_addr);
+
+    let listener = TcpListener::bind(local_addr).await?;
+    let (mut stream, _sender) = listener.accept().await?;
+    let out_file = File::create("out.bin").await?;
+    copy(&mut stream, &mut BufWriter::new(out_file)).await?;
     Ok(())
 }
 
-async fn make_node() -> Result<(Endpoint, IncomingConnections)> {
+/*async fn make_node() -> Result<(Endpoint, IncomingConnections)> {
     let internal_ip = local_ip().expect("Couldn't get internal IP.");
     let port = port_scanner::request_open_port().expect("Unable to find an available port.");
     let external_ip = public_ip::addr().await;
@@ -119,4 +135,4 @@ async fn make_node() -> Result<(Endpoint, IncomingConnections)> {
     .await?;
 
     Ok((node, incoming_connections))
-}
+}*/
