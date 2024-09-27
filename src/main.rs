94ffe505fa467aa43d28da77717f05d3d0276adb
Mason Dill <mdill@cervis.net>
2024-09-27 16:01:19 -0400

    added visibility option

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use clap::error;
 use clap::{builder::PossibleValue, Parser};
-use tokio::{io::AsyncReadExt, io::AsyncWriteExt, net::TcpListener, net::TcpSocket, net::TcpStream};
-use std::net::SocketAddr;
-use std::io;
+use tokio::{io::AsyncReadExt, io::AsyncWriteExt, net::TcpListener, net::TcpStream};
+use serde_json::Value;
 
 #[derive(Parser)]
 struct Args {   
@@ -11,14 +9,24 @@ struct Args {
         PossibleValue::new("client").alias("rx")]))]
     role: String,
 
-    #[arg(short, long)]
+    #[arg(index(2), required(true))]
     file: Option<String>,
 
     #[arg(short('p'), long, required_if_eq("role", "server"))]
     port: Option<u16>,
 
-    #[arg(short('a'), long, required_if_eq("role", "client"))]
-    peer_address: Option<String>,
+    #[arg(short('s'), long, required_if_eq("role", "client"))]
+    peer_socket: Option<String>,
+
+    #[arg(
+        short,
+        long,
+        value_parser([
+        PossibleValue::new("public").alias("WWW"),
+        PossibleValue::new("private").alias("LAN")]),
+        default_value("public")
+    )]
+    visibility: String,
 }
 
 const WORKER_THREADS: usize = 1;
@@ -64,8 +72,8 @@ async fn write_file(file : &str, contents : Vec<u8>) -> Result<(), Box<dyn std::
 }
 
 async fn trasmit(args : &Args) -> Result<(), Box<dyn std::error::Error>> {
-    let peer_address: &Option<String> = &args.peer_address;
-    let mut stream = TcpStream::connect(args.peer_address.as_ref().unwrap()).await?;
+    let peer_address: &Option<String> = &args.peer_socket;
+    let mut stream = TcpStream::connect(args.peer_socket.as_ref().unwrap()).await?;
     
     // Load the data from the file
     let mut contents = read_file(args.file.as_ref().unwrap()).await?;
@@ -74,23 +82,66 @@ async fn trasmit(args : &Args) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn receive(args : &Args) -> io::Result<()> {
-    let socket_address = format!("127.0.0.1:{}", args.port.unwrap());
-    let listener = TcpListener::bind(&socket_address).await?;
-    println!("Listening on: {}", listener.local_addr()?);
+async fn download_from_tcp_stream(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+    let mut buf = [0; 1024];
+    let n = stream.read(&mut buf).await?;
+    println!("{}", std::str::from_utf8(&buf[..n]).unwrap());
+    Ok(())
+}
 
-    //let file: &str = args.file.as_ref().unwrap();
-    loop{
-        let (mut socket, _) = listener.accept().await?;
-        println!("Accepted connection from {}", socket.peer_addr()?);
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
-            let n = socket.read(&mut buf).await.unwrap();
-            
-            // write the data to the file
-            //write_file(&file, buf.to_vec());
-            print!("{}", std::str::from_utf8(&buf[..n]).unwrap());
+const LOCAL_HOST: &str = "0.0.0.0";
+async fn receive(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
+    let private_socket_address = format!("{}:{}", LOCAL_HOST, args.port.unwrap());
+    let listener = TcpListener::bind(&private_socket_address)
+        .await
+        .unwrap_or_else(|e| {
+            eprintln!("Failed to bind to port {} - {}", args.port.unwrap(), e);
+            std::process::exit(1); // Exit if binding fails
         });
-    }
     
+    let public_ip: &str = match args.visibility.as_str() {
+        "public" => &fetch_public_ip().await?,
+        "private" => &fetch_private_ip().await?,
+        _ => unreachable!(),
+    };
+    let public_socket_address = format!("{}:{}", public_ip, args.port.unwrap());
+    println!("Listening on socket {}", &public_socket_address);
+
+    let (socket, _) = listener.accept().await?;
+    println!("Accepted connection from {}", socket.peer_addr()?);
+    tokio::spawn(async move {
+        download_from_tcp_stream(socket).await.unwrap();
+    }).await?;
+
+    println!("Transmission complete!");
+    Ok(())
+}
+
+async fn fetch_private_ip() -> Result<String, Box<dyn std::error::Error>> {
+    let output = std::process::Command::new("ipconfig")
+        .output()
+        .expect("Failed to execute ipconfig");
+    let output = String::from_utf8(output.stdout).unwrap();
+    let ip = output
+        .lines()
+        .find(|line| line.contains("IPv4 Address"))
+        .unwrap()
+        .split(':')
+        .last()
+        .unwrap()
+        .trim();
+
+    return Ok(ip.to_string());
+}
+
+async fn fetch_public_ip() -> Result<String, Box<dyn std::error::Error>> {
+    let mut stream = TcpStream::connect("api.ipify.org:80").await?;
+    stream.write_all(b"GET /?format=json HTTP/1.1\r\nHost: api.ipify.org\r\n\r\n").await?;
+    let mut buf = [0; 1024];
+    let n = stream.read(&mut buf).await?;
+    let response = std::str::from_utf8(&buf[..n]).unwrap().lines().last().unwrap();
+
+    let json: Value = serde_json::from_str(response)?;
+    let ip: String = json["ip"].as_str().unwrap().to_string();
+    return Ok(ip);
 }
